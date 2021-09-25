a4b8591786dca1c4e3e29ad74c1fac779b0f5685
omabney <om.gopalrao@gmail.com>
2021-09-24 20:47:51 -0600

    Completed Week 1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const links = [
     {
-        label : "Week 1",
-        url: "week1/index.html"
+    label: "Week 1",
+    url: "week1/index.html"
     },
-   // {
-    //    label : "Week 2",
-    //    url: "week2/index.html"
-    //}
+    {
+        label: "Week 2",
+        url: "week2/index.html"
+    }
 ]
 
-
-let htmllist = document.getElementById("weekly-content");
-
-for(let i=0;i<links.length;i++){
-    let li =document.createElement('li');
+let htmlList = document.getElementById("weekly-Content");
+for(let i=0; i< links.length;i++){
+    let li = document.createElement('li');
     let a = document.createElement('a');
-    a.setAttribute("href" , links[i]["url"]);
+    a.setAttribute("href",links[i]["url"]);
     a.innerText = links[i]["label"];
     li.appendChild(a);
-    htmllist.appendChild(li);
+    htmlList.appendChild(li);
 }
+
+   
